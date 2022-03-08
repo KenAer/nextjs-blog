5d1f61fae068e39c2ce34c3bf2e81ea1e37d14a9
Ken Aernouts <kaernouts@fhps.net>
2022-03-08 15:43:44 -0500

    profile page looks good

---
+++ pages/posts/first-post.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../../components/layout'
+
+export default function FirstPost() {
+    return (
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <h1>First Post</h1>
+            <p>
+            Spicy jalapeno bacon ipsum dolor amet salami strip steak ribeye bacon. Leberkas prosciutto pork belly brisket. Ground round shank capicola ham hock jowl pork chuck. Boudin filet mignon cupim, tenderloin alcatra tongue corned beef kielbasa bacon andouille salami sausage tail doner.
+
+Pork loin prosciutto boudin tongue sirloin. Corned beef tri-tip picanha leberkas beef bacon. Ham bresaola boudin pork loin spare ribs jerky strip steak tri-tip. Pork beef ribs bresaola fatback beef kevin chislic tenderloin chicken prosciutto. Burgdoggen beef turkey, t-bone pancetta biltong ground round leberkas ham hock kielbasa pork loin.
+            </p>
+        </Layout>
+    )
+}
